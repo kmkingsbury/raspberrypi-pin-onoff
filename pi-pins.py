b7f5093f23b6a7b8bbf27a4bc1a84cd71f03a711
Kevin Kingsbury <kkingsbury@gmail.com>
2015-12-24 21:21:09 +0000

    Now takes pins from the input, can take a list of comma seperated and hypthen range of values

--- pi-pins.py
+++ pi-pins.py
@@ -1,26 +1,61 @@
 import os
 import RPi.GPIO as GPIO
 import time
+import re
 
-GPIO.setmode(GPIO.BCM) 
+def validate(inpins):
+    outpins = []
+    #2 to 27 is valid pin numbers
+    for (i,item) in enumerate(inpins):
+        #print "Checking: " + str(item)
+        if int(item) < 2 or int(item) > 27:
+            print "Item " + str(item) + " is not valid, removing."
+        else:
+            outpins.append(int(item))
+    return outpins
+
+def split_pins(inpins):
+  listout = []
+  list1 = inpins.split(',')
+  for item in list1:
+    #print "item: " + str(item)
+    if re.search('-', item):
+        r = item.split('-')
+        #print "match in "+ item
+        for num in range(int(r[0]),int(r[1])+1):
+            #print "R:"+ str(num)
+            listout.append(int(num))
+    else:
+        #print "no match " + item
+        listout.append(int(item))
+  return listout
+GPIO.setmode(GPIO.BCM)
 
 
-mypin = 18
+strpins = raw_input('Enter Pins (ex: 14,15-17,18))')
 
-GPIO.setup(mypin, GPIO.OUT)
-GPIO.output(mypin, GPIO.HIGH)
+pins = split_pins(strpins)
+pins = validate(pins)
+pins.sort();
+
+print "Pins: "
+print '[%s]' % ', '.join(map(str, pins))
+
+# Can take list so send list of pins to set HIGH:
+#for mypin in listpins:
+# print "Setting: " + str(mypin)
+GPIO.setup(pins, GPIO.OUT)
+GPIO.output(pins, GPIO.HIGH)
 
 runner = True
-
 try:
     while (runner == True):
       time.sleep(1) #set to whatever
 
 except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
-    print "\nKilling Thread..."    
+    print "\nKilling Thread..."
     runner = False
 
-GPIO.output(mypin, GPIO.LOW)
+GPIO.output(pins, GPIO.LOW)
+
 GPIO.cleanup()
-
-
